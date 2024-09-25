15685220cd954cd25294691a70864532fff146f0
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-26 04:45:28 +0800

    Class student and different management option construction

--- main.rb
+++ main.rb
@@ -1,1 +1,32 @@
-puts "Hello World"
+require_relative 'student'
+
+def main_loop
+  loop do
+    puts "(1) Student Management
+(2) Teacher Management
+(3) Course Management
+(4) Subject Management
+(5) Exit
+"
+
+    print "Enter the number:"
+    option = gets.chomp
+
+    case option
+    when '1'
+      Student.options
+    when '2'
+      Teacher.options
+    when '3'
+      Course.options
+    when '4'
+      Subject.options
+    when '5'
+      puts "\nSystem exit."
+      exit
+    end
+  end
+end
+
+# Start of Code
+main_loop()
